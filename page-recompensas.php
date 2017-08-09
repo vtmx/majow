c14c0138a44c799be2b9b37c5d8a731a93849645
Vitor Melo <vtmx22@gmail.com>
2017-08-09 15:56:58 -0300

    Página de recompensa.

--- page-recompensas.php
+++ page-recompensas.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
-<iframe
-	src="https://hellojuniper.com/majowww/"
-	width="1168"
-	height="910"
-	frameborder="0"
-	scrolling="no"
-	allowfullscreen>
-</iframe>
+	<iframe
+		src="https://hellojuniper.com/majowww/"
+		width="1168"
+		height="910"
+		frameborder="0"
+		scrolling="no"
+		allowfullscreen>
+	</iframe>
 
 <?php get_footer(); ?>
