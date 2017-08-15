3a0482aecf1cd859faedfe217e3d0dda8a10a131
Vitor Melo <vtmx22@gmail.com>
2017-08-15 17:04:13 -0300

    Add favicons.

--- header.php
+++ header.php
@@ -8,6 +8,14 @@
 	<!-- Styles -->
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dist/css/style.min.css">
 
+	<!-- Favicons -->
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/favicon-16x16.png">
+	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/manifest.json">
+	<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="theme-color" content="#ffffff">
+
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
